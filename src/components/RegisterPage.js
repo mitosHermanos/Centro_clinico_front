f8f7c1e31e819f525523479f602b3e97323de78f
ognjengaric <40010757+ognjengaric@users.noreply.github.com>
2019-11-17 19:03:23 +0100

    ui created

---
+++ src/components/RegisterPage.js
@@ -0,0 +1,130 @@
+import React, {useState} from 'react';
+import {Container, Form, Col, Button} from 'react-bootstrap'
+
+function RegisterPage(){
+
+    return(
+        <Container>
+            <div className='register-div'>
+                <h2>Create a patient account</h2>
+                <Form>
+                <Form.Row>
+                    <Form.Group as={Col} md="6">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                required
+                                id="email"
+                                type="email"
+                                placeholder="Email"
+                            />
+                        </Form.Group>
+                        <Form.Group as={Col} md="6">
+                            <Form.Label>Social security number</Form.Label>
+                            <Form.Control
+                                required
+                                id="socialSecurityNumber"
+                                type="text"
+                                placeholder="Social security number"
+                            />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                    <Form.Group as={Col} md="6">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                required
+                                id="password"
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Form.Group>
+                        <Form.Group as={Col} md="6">
+                            <Form.Label>Repeat password</Form.Label>
+                            <Form.Control
+                                required
+                                id="repeatPassword"
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                    <Form.Group as={Col} md="4">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control
+                                required
+                                id="name"
+                                type="text"
+                                placeholder="Name"
+                            />
+                        </Form.Group>
+                        <Form.Group as={Col} md="4">
+                            <Form.Label>Surname</Form.Label>
+                            <Form.Control
+                                required
+                                id="surname"
+                                type="text"
+                                placeholder="Surname"
+                            />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                    <Form.Group as={Col} md="8">
+                            <Form.Label>Street name</Form.Label>
+                            <Form.Control
+                                required
+                                id="street"
+                                type="text"
+                                placeholder="Street name"
+                            />
+                        </Form.Group>
+                        <Form.Group as={Col} md="4">
+                            <Form.Label>Street number</Form.Label>
+                            <Form.Control
+                                required
+                                id="number"
+                                type="text"
+                                placeholder="Street number"
+                            />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col} md="4">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control
+                                required
+                                id="city"
+                                type="text"
+                                placeholder="City"
+                            />
+                        </Form.Group>
+                        <Form.Group as={Col} md="4">
+                            <Form.Label>Postcode</Form.Label>
+                            <Form.Control
+                                required
+                                id="postcode"
+                                type="text"
+                                placeholder="Postcode"
+                            />
+                        </Form.Group>
+                        <Form.Group as={Col} md="4">
+                            <Form.Label>Country</Form.Label>
+                            <Form.Control
+                                required
+                                id="country"
+                                type="text"
+                                placeholder="Country"
+                            />
+                        </Form.Group>
+                    </Form.Row>
+                    <div className="text-center">
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                    </div>
+                </Form>
+            </div>
+        </Container>
+    );
+}
+export default RegisterPage; 
