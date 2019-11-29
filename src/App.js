1335593c7c4ef4175f1613ae58977131b98467fc
ognjengaric <40010757+ognjengaric@users.noreply.github.com>
2019-11-29 14:33:52 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-import './App.css';
-=======
 import { Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage.js'
 import RegisterPage from './components/RegisterPage.js'
@@ -14,27 +10,11 @@ import ClinicCentAdmin from './components/ClinicCentAdmin.js';
 import CCPerson from './components/CCPerson.js';
 import EditClinicProfile from './components/EditClinicProfile.js';
 import EditClinicAdminProfile from './components/EditClinicAdminProfile.js';
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
+
       <Route exact path="/" component={LoginPage} />
       <Route path="/register" component={RegisterPage} />
       <Route path="/patientInfo" component={PatientInfo} />
@@ -42,7 +22,6 @@ function App() {
       <Route path="/clinicCentAdmin" component={CCPerson}/>
       <Route path="/editClinicProfile" component={EditClinicProfile}/>
       <Route path="/editClinicAdminProfile" component={EditClinicAdminProfile} />
->>>>>>> Stashed changes
     </div>
   );
 }
