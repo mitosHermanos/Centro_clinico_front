36abdee4c54420c52a837f62b60d42d284d0312c
ognjengaric <40010757+ognjengaric@users.noreply.github.com>
2019-11-29 15:10:20 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
-import logo from './logo.svg';
-import './App.css';
->>>>>>> 2fe75bd2c1180ffa69ae815d12d6c77f25a07754
+import './styles/App.css';
 import { Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage.js'
 import RegisterPage from './components/RegisterPage.js'
@@ -16,21 +12,10 @@ import ClinicCentAdmin from './components/ClinicCentAdmin.js';
 import CCPerson from './components/CCPerson.js';
 import EditClinicProfile from './components/EditClinicProfile.js';
 import EditClinicAdminProfile from './components/EditClinicAdminProfile.js';
-<<<<<<< HEAD
-=======
-
->>>>>>> 2fe75bd2c1180ffa69ae815d12d6c77f25a07754
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-
-=======
->>>>>>> 2fe75bd2c1180ffa69ae815d12d6c77f25a07754
-      <Route exact path="/" component={LoginPage} />
-      <Route path="/register" component={RegisterPage} />
-      <Route path="/patientInfo" component={PatientInfo} />
       <Route exact path="/" component={LoginPage} />
       <Route path="/register" component={RegisterPage} />
       <Route path="/registerClinic" component={RegisterClinic}/>
