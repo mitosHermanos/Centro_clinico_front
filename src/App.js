605b39a113919969265096da3d9378953819e50b
ognjengaric <40010757+ognjengaric@users.noreply.github.com>
2019-11-29 14:17:36 +0100

    initial patient info ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import logo from './logo.svg';
 import './App.css';
+=======
+import { Route } from 'react-router-dom';
+import LoginPage from './components/LoginPage.js'
+import RegisterPage from './components/RegisterPage.js'
+import RegisterClinic from './components/RegisterClinic.js'
+import PatientInfo from './components/PatientInfo.js'
+
+import './styles/App.css';
+import ClinicCentAdmin from './components/ClinicCentAdmin.js';
+import CCPerson from './components/CCPerson.js';
+import EditClinicProfile from './components/EditClinicProfile.js';
+import EditClinicAdminProfile from './components/EditClinicAdminProfile.js';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +34,15 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+      <Route exact path="/" component={LoginPage} />
+      <Route path="/register" component={RegisterPage} />
+      <Route path="/patientInfo" component={PatientInfo} />
+      <Route path="/registerClinic" component={RegisterClinic}/>
+      <Route path="/clinicCentAdmin" component={CCPerson}/>
+      <Route path="/editClinicProfile" component={EditClinicProfile}/>
+      <Route path="/editClinicAdminProfile" component={EditClinicAdminProfile} />
+>>>>>>> Stashed changes
     </div>
   );
 }
---
+++ src/components/LoginPage.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import {Form, Button, Container} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import {serviceConfig} from '../appSettings.js'
+
+class LoginPage extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            _email : "",
+            _password : ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    handleChange(e) {
+        const { id, value } = e.target;
+        this.setState({ [id]: value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.login();
+    }
+
+    login(){
+        const {_email, _password} = this.state;
+
+        const loginData = {
+            email : _email,
+            password : _password
+        }
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(loginData)
+        };
+
+        fetch(`${serviceConfig.baseURL}/auth/login`, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                return Promise.reject(response);
+            }
+            return response.statusText;
+        })
+        .then(() => {
+            this.props.history.push('/');
+        })
+        .catch(response => {
+            return response.json();
+        })
+        .then((data) => {
+            alert(data.message);
+        });
+    }
+
+    render(){
+        const {_email, _password} = this.state;
+
+        return(
+            <Container>
+                    <div className='login-div'>
+                        <h2>Centro clinico</h2> 
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Group>
+                                <Form.Label>
+                                    Email
+                                </Form.Label>
+                                <Form.Control
+                                    required
+                                    id="_email"
+                                    value={_email}
+                                    type="text"
+                                    placeholder="Email"
+                                    onChange = {this.handleChange}
+                                />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>
+                                    Password
+                                </Form.Label>
+                                <Form.Control
+                                    required
+                                    id="_password"
+                                    value={_password}
+                                    type="password"
+                                    placeholder="Password"
+                                    onChange = {this.handleChange}
+                                />
+                            </Form.Group>
+                            <div className="text-center">
+                                <Button variant="primary" type="submit">
+                                    Log in
+                                </Button>
+                            </div>
+                        </Form>
+                    </div>
+                    <div className="text-center">
+                            Don't have an account?
+                            <br/>   
+                            <Link to="/register">
+                                Sign up
+                            </Link>
+                    </div>
+            </Container>
+        );
+    }
+}
+export default LoginPage;
